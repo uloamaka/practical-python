4f1687082a96ecd72bebaf68276e9838aec3cece
Ebite God'sgift Uloamaka <godsgiftuloamaka235@gmail.com>
2024-03-06 20:17:14 +0100

    solved the bounce exercise

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,13 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100
+bum_bounce = 3/5
+times_of_bounce = 0
+
+while times_of_bounce  < 10:
+  times_of_bounce = times_of_bounce  + 1
+  height =  height * bum_bounce
+  print(round(height, 4))
+
+
